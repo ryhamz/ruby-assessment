7550ec9bb6e31a47cfe19972b6f077859d8e7de8
ryhamz <ryhamz@gmail.com>
2015-05-29 21:10:11 -0500

    level 1

--- spec/level_1_spec.rb
+++ spec/level_1_spec.rb
@@ -2,7 +2,8 @@ describe "Level 1" do
 	describe "Arrays" do
 		it "adds an element to an array" do 
 			array = ["Blake","Steven","Jeff"]
-  		# add an element to the array
+  		        # add an element to the array
+                        array.push("Ryan")
 			expect(array.count).to eq(4)
 		end
 
@@ -11,19 +12,21 @@ describe "Level 1" do
 			expect(STDOUT).to receive(:puts).with("Blake")
 			expect(STDOUT).to receive(:puts).with("Steven")
 			expect(STDOUT).to receive(:puts).with("Jeff")
-
+  
 			# iterate over each element in the array and puts it
+                        array.each {|x| puts x}
+                        
 		end
 
 		it "returns the value at index 0" do
 			array = ["Blake","Steven","Jeff"]
-			element_1 = :banana # replace banana with the code that finds the first element
+			element_1 = array[0] # replace banana with the code that finds the first element
 			expect(element_1).to eq("Blake") 
 		end
 
 		it "returns the user at an index" do
 			array = ["Blake","Steven","Jeff"]
-			jeff_index = :banana # replace banana with the code that finds Jeff's index
+			jeff_index = array.index {|x| x == "Jeff"} # replace banana with the code that finds Jeff's index
 			expect(jeff_index).to eq(2)
 		end
 	end
